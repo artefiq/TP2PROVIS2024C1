933708fd46070c4a30cee595b6a21ea1db482c48
taufiq <taufiqhidayat140504@gmail.com>
2024-03-21 17:28:21 +0700

    second submit

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -10,12 +10,74 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        backgroundColor: const Color(0xffffffff),
         appBar: AppBar(
           title: const Text("Booking"),
         ),
-        body: const Center(
-          child: Text("Hello World!"),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Hospital & Medical Services",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20), // Spacer
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 150,
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: List.generate(
+                              10,
+                              (index) => Container(
+                                width: 120,
+                                height: 150,
+                                margin: EdgeInsets.only(right: 20),
+                                decoration: BoxDecoration(
+                                  color: Colors.white, // Changed to white
+                                  borderRadius: BorderRadius.circular(20),
+                                  border: Border.all(
+                                    color: const Color(
+                                        0xff090F47), // Outline color
+                                    width: 3, // Outline width
+                                  ),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "Content Box $index",
+                                    style: TextStyle(
+                                      color:
+                                          const Color(0xff090F47), // Text color
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
         bottomNavigationBar: MyBottomNavigationBar(),
       ),
@@ -42,24 +104,25 @@ class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
     return BottomNavigationBar(
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
-          icon: Icon(Icons.home_max, size : 24),
+          icon: Icon(Icons.home_max, size: 24),
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.book_outlined, size : 24),
+          icon: Icon(Icons.book_outlined, size: 24),
           label: 'Booking',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.monitor_heart_outlined, size : 24),
+          icon: Icon(Icons.monitor_heart_outlined, size: 24),
           label: 'Activity',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.account_circle_outlined, size : 24),
+          icon: Icon(Icons.account_circle_outlined, size: 24),
           label: 'Account',
         ),
       ],
       currentIndex: _selectedIndex,
-      selectedItemColor: const Color(0xff0F1035), // Change this to your preferred color
+      selectedItemColor:
+          const Color(0xff0F1035), // Change this to your preferred color
       unselectedItemColor: Colors.grey, // Set the unselected item color here
       showUnselectedLabels: true,
       onTap: _onItemTapped,
