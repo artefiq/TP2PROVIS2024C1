7dcb717f0cc254751e55141046ef158b5fc959ed
taufiq hidayat <taufiqhidayat140504@gmail.com>
2024-03-29 09:26:21 +0700

    Final

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -81,10 +81,26 @@ class MyApp extends StatelessWidget {
                             imageUrls: [
                               'https://blu-djpb.kemenkeu.go.id/images/5_1624092610504da276891e5453b002c784f824f582.jpg',
                               'https://rsgm.unpad.ac.id/wp-content/uploads/2021/03/LOGO-RSGM-UNPAD.png',
-                              'https://via.placeholder.com/150?text=Image3',
-                              'https://via.placeholder.com/150?text=Image4',
-                              'https://via.placeholder.com/150?text=Image5',
-                              // Add more image URLs here for the first list
+                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSigPOs6PJiwnZH0Vg77SU7gwYIHUmSgRQVCJzxQ4iN71dfUhp9jJpZ2ywezbfKSwBLng&usqp=CAU',
+                              'https://rsborromeus.com/wp-content/uploads/2017/02/IMG_0076.jpg',
+                            ],
+                            imageTexts: [
+                              'Rumah Sakit Hasan Sadikin',
+                              'Rumah Sakit Gigi dan Mulut Unpad',
+                              'Klinik AVA\nDental Aesthetic',
+                              'Rumah Sakit\nSanto Borromeus',
+                            ],
+                            additionalTexts: [
+                              'General Hospital',
+                              'Specialty Hospital',
+                              'Clinic',
+                              'General Hospital',
+                            ],
+                            distances: [
+                              '1.2', // Distance for the first image
+                              '2.5', // Distance for the second image
+                              '3.8', // Distance for the third image
+                              '4.1', // Distance for the fourth image
                             ],
                           ),
                         ),
@@ -132,14 +148,18 @@ class MyApp extends StatelessWidget {
                         height: 150,
                         child: SingleChildScrollView(
                           scrollDirection: Axis.horizontal,
-                          child: ImageListView(
+                          child: ImageTextListView(
                             imageUrls: [
-                              'https://via.placeholder.com/150?text=Image6',
-                              'https://via.placeholder.com/150?text=Image7',
-                              'https://via.placeholder.com/150?text=Image8',
-                              'https://via.placeholder.com/150?text=Image9',
-                              'https://via.placeholder.com/150?text=Image10',
-                              // Add more image URLs here for the second list
+                              'https://www.clipartmax.com/png/middle/100-1006207_consultation-%E2%80%9C-general-practitioner-vector.png',
+                              'https://i.pngimg.me/thumb/f/720/m2i8N4b1i8N4m2b1.jpg',
+                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScCDTSa-4P5ikykVPoJyedCGUUn764kZc6cg&usqp=CAU',
+                              'https://img.freepik.com/free-vector/brain-chemistry-concept-illustration_114360-10136.jpg',
+                            ],
+                            imageTexts: [
+                              'General Praciticioner',
+                              'Pediatric',
+                              'Dentist',
+                              'Neurologist',
                             ],
                           ),
                         ),
@@ -159,8 +179,17 @@ class MyApp extends StatelessWidget {
 
 class ImageListView extends StatelessWidget {
   final List<String> imageUrls;
+  final List<String> imageTexts;
+  final List<String> additionalTexts;
+  final List<String> distances; // List of distances
 
-  const ImageListView({Key? key, required this.imageUrls}) : super(key: key);
+  const ImageListView({
+    Key? key,
+    required this.imageUrls,
+    required this.imageTexts,
+    required this.additionalTexts,
+    required this.distances,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -169,23 +198,131 @@ class ImageListView extends StatelessWidget {
         imageUrls.length,
         (index) => Container(
           width: 120,
-          height: 150,
+          height: 200,
           margin: EdgeInsets.only(right: 20),
           decoration: BoxDecoration(
-            color: Colors.white, // Changed to white
+            color: Colors.white,
             borderRadius: BorderRadius.circular(20),
             border: Border.all(
-              color: const Color(
-                0xff090F47,
-              ), // Outline color
-              width: 3, // Outline width
+              color: const Color(0xff090F47),
+              width: 3,
             ),
           ),
-          child: Center(
-            child: Image.network(
-              imageUrls[index],
-              width: 80, // Adjust size as needed
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20, bottom: 5, left: 10),
+                    child: Image.network(
+                      imageUrls[index],
+                      width: 90,
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(height: 5),
+                      Text(
+                        imageTexts[index],
+                        style: TextStyle(fontSize: 13),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        additionalTexts[index],
+                        style: TextStyle(fontSize: 8),
+                      ),
+                      SizedBox(height: 0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.location_pin, size: 10), // Distance icon
+                          SizedBox(width: 0),
+                          Text(
+                            '${distances[index]} km', // Distance text
+                            style: TextStyle(fontSize: 8),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ImageTextListView extends StatelessWidget {
+  final List<String> imageUrls;
+  final List<String> imageTexts;
+
+  const ImageTextListView({
+    Key? key,
+    required this.imageUrls,
+    required this.imageTexts,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: List.generate(
+        imageUrls.length,
+        (index) => Container(
+          width: 120,
+          height: 200,
+          margin: EdgeInsets.only(right: 20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: const Color(0xff090F47),
+              width: 3,
             ),
+          ),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 5, left: 17),
+                    child: Image.network(
+                      imageUrls[index],
+                      width: 80.0,
+                      height: 80.0,
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        imageTexts[index],
+                        style: TextStyle(fontSize: 13),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -229,9 +366,8 @@ class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
         ),
       ],
       currentIndex: _selectedIndex,
-      selectedItemColor:
-          const Color(0xff0F1035), // Change this to your preferred color
-      unselectedItemColor: Colors.grey, // Set the unselected item color here
+      selectedItemColor: const Color(0xff0F1035),
+      unselectedItemColor: Colors.grey,
       showUnselectedLabels: true,
       onTap: _onItemTapped,
     );
